2b664eacb35fd6ea8123ea01685fbc0c11b8694d
deepak31 <deepak.it22@bitsathy.ac.in>
2024-07-28 01:31:45 +0530

    updated

---
+++ main.js
@@ -0,0 +1,55 @@
+// ----- NAVIGATION BAR FUNCTION -----
+function myMenuFunction() {
+    var menuBtn = document.getElementById("myNavMenu");
+    menuBtn.classList.toggle("responsive");
+}
+
+window.onscroll = function() { headerShadow(); };
+
+function headerShadow() {
+    const navHeader = document.getElementById("header");
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
+        navHeader.style.height = "70px";
+        navHeader.style.lineHeight = "70px";
+    } else {
+        navHeader.style.boxShadow = "none";
+        navHeader.style.height = "90px";
+        navHeader.style.lineHeight = "90px";
+    }
+}
+
+var typingEffect = new Typed(".typedText", {
+    strings: ["Full stack developer", "UI Designer", "Web Developer"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 2000
+});
+
+const sections = document.querySelectorAll('section[id]');
+const navLinks = document.querySelectorAll('.nav-menu a');
+
+function scrollActive() {
+    const scrollY = window.scrollY;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        const sectionId = current.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link');
+        } else {
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link');
+        }
+    });
+}
+
+window.addEventListener('scroll', scrollActive);
+
+document.querySelectorAll('.circle').forEach(circle => {
+    const percent = circle.getAttribute('data-percent');
+    const angle = (percent / 100) * 360;
+    circle.style.background = `conic-gradient(#00bcd4 ${angle}deg, #283e51 ${angle}deg 360deg)`;
+});
